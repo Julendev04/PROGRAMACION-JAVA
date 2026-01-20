895a9f8820647fd6199b3590497eb5514cbc0ff6
Julen Ruiz De Viñaspre <ruizdevinaspre4@gmail.com>
2026-01-20 08:16:12 +0100

    Actualizaciones ejercicios

--- ProgramazioaArrays/UD6/src/UD6_Tanda1/OpArray.java
+++ ProgramazioaArrays/UD6/src/UD6_Tanda1/OpArray.java
@@ -1,13 +1,68 @@
 package UD6_Tanda1;
 
+import java.util.Arrays;
+
+import Arrays.Kontsola;
+
 public class OpArray {
-	int [] notak;
-	static final int[] zenb_debekatu;
+	int [] arr_zenb;
+	static final int[] zenb_debekatu= {0, -1, -999};
+	int n=0;
 	
-	public OpArray(int[] notak) {
-		super();
-		this.notak = notak;
+	public OpArray() {
+		this.arr_zenb = new int[zenb_debekatu.length];
 	}
+	
+	public OpArray(int tam) {
+		this.arr_zenb=new int[tam];
+	}
+	
+	public void arrayaIkusi() {
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr_zenb[i]+"\t");
+		}
+	}
+	public void arrayaKargatu() {
+		while (true) {
+			System.out.println("Array-a osatzeko zenbakiak sartu:");
+			int zenb=Kontsola.irakurriInt();
+			if(zenb==0) {
+				break;
+			}
+			
+			for (int j = 0; j < zenb_debekatu.length; j++) {
+			    if (zenb == zenb_debekatu[j]) {
+			        System.out.println("Zenbaki debekatua! Saiatu berriro:");
+			        zenb=Kontsola.irakurriInt();
+			    }
+			}
+			
+			arr_zenb[n] = zenb;
+			n++;
+			
+		}
+		
+	}
+	
+
+	public static void main(String[] args) {
+		OpArray miArray= new OpArray();
+		miArray.arrayaKargatu();
+		miArray.arrayaIkusi();
+		miArray.toString();
+		
+		
+		
+	}
+
+	@Override
+	public String toString() {
+		return "OpArray [arr_zenb=" + Arrays.toString(arr_zenb) + "]";
+	}
+	
+	
+
+	
 	
 	
 
--- ProgramazioaArrays/UD6/src/UD6_Tanda1/ZenbakiaAurkitu.java
+++ ProgramazioaArrays/UD6/src/UD6_Tanda1/ZenbakiaAurkitu.java
@@ -1,36 +1,39 @@
 package UD6_Tanda1;
 
-import java.util.Iterator;
-
 import Arrays.Kontsola;
 
 public class ZenbakiaAurkitu {
-	public static int ZenbakiaAurkitu(int[] arr) {
-		System.out.println("Esan zenbaki bat ea sartuta dagoen array-an");
-		int zenb=Kontsola.irakurriInt();
-		int posizioa=-1;
-		int beheLim=0;
-		int goiLim=arr.length-1;
-		int erdia=(beheLim+goiLim)/2;
-		
-		while(posizioa==-1 && erdia!=zenb) {
-			if (erdia<zenb) {
-				beheLim=erdia;
-				goiLim=arr.length-1;
-			} else if (erdia>zenb) {
-				beheLim=0;
-				goiLim=erdia;
-			} else {
-				posizioa=1;
-			}
-		}
-		
-		return posizioa;
-	}
-	
-	public static void main(String[] args) {
-		int[] arr={12, 10, 5, 1, 7, 22};
-		ZenbakiaAurkitu(arr);
-	}
-	
+
+    public static int ZenbakiaAurkitu(int[] arr) {
+        System.out.println("Esan zenbaki bat ea sartuta dagoen array-an:");
+        int zenb = Kontsola.irakurriInt();
+        int beheLim = 0;
+        int goiLim = arr.length - 1;
+        int posizioa = -1;
+        while (beheLim <= goiLim) {
+            int erdia = (beheLim + goiLim) / 2;
+
+            if (arr[erdia] == zenb) {
+                posizioa = erdia;
+                break;
+            } else if (arr[erdia] < zenb) {
+                beheLim = erdia+1;
+            } else {
+                goiLim=erdia-1;
+            }
+        }
+
+        return posizioa;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 10, 12, 22}; 
+        int pos = ZenbakiaAurkitu(arr);
+
+        if (pos != -1) {
+            System.out.println("Zenbakia aurkitu da posizioan: " + pos);
+        } else {
+            System.out.println("Zenbakia ez da aurkitu.");
+        }
+    }
 }
